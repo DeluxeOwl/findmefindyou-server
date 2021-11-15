05c9be5f3a7ab8cba7544007540fadc708711357
DeluxeOwl <nospamplease0@protonmail.com>
2021-11-15 16:58:08 +0200

    Added /friends endpoint

--- main.py
+++ main.py
@@ -10,11 +10,14 @@ from nanoid import generate
 config = dotenv_values(".env")
 app = FastAPI()
 
-# !!! Note: this isn't meant to be secure
-# could also use a cache ...
-
 
 async def verify_token(x_key: str = Header(None)):
+    """ 
+    Note: this isn't meant to be secure
+    could also use a cache ...
+    You have to pass a valid key
+    curl -H "X-Key: your_key" http://127.0.0.1:8000/
+    """
     if x_key is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
     res = await conn.fetchrow(
@@ -28,8 +31,10 @@ async def verify_token(x_key: str = Header(None)):
 
 @app.on_event('startup')
 async def startup():
-    # One connection is enough for this project as a global variable
-    # Pooling should be used instead (+ don't use a global variable)
+    """
+    One connection is enough for this project as a global variable
+    Pooling should be used instead (+ don't use a global variable)
+    """
     global conn
     conn = await asyncpg.connect(user=config['POSTGRES_USER'], password=config['POSTGRES_PASSWORD'],
                                  host=config['POSTGRES_HOST'], port=config['POSTGRES_PORT'])
@@ -42,7 +47,11 @@ async def shutdown():
 
 @app.get("/init")
 async def get_creds():
-    # maximum length is 18 for the name
+    """
+    Display name maximum length is 18
+    Unique key maximum length is 12
+    """
+
     display_name = petname.Generate(3, separator='-', letters=5)
     unique_key = generate(size=12)
 
@@ -52,8 +61,23 @@ async def get_creds():
     }
 
 
-# You have to pass a valid key
-# curl -H "X-Key: your_key" http://127.0.0.1:8000/
+@app.get("/friends")
+async def get_friends(user_info: dict = Depends(verify_token)):
+    res = await conn.fetch(
+        """
+        select * from users where user_id in
+        (select f.friend_id 
+            from users u 
+            inner join friends f 
+                on u.user_id = f.user_id 
+        where unique_key=$1);
+        """,
+        user_info['unique_key']
+    )
+
+    return [dict(entry) for entry in res]
+
+
 @app.get("/")
 async def get_root(user_info: dict = Depends(verify_token)):
 
